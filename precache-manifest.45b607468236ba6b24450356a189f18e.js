c3724c4e45d39ae7cf128f1fd31216763e6b9898
Drainkoron <lukaasatiani73@gmail.com>
2020-08-18 21:00:31 +0400

    init

---
+++ precache-manifest.45b607468236ba6b24450356a189f18e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aee1a95a3e3f96ccd47fbdbe9ab35ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a4a54d0d0262e6b427",
+    "url": "/static/css/main.0812f62b.chunk.css"
+  },
+  {
+    "revision": "4aae4a71f611d614f63b",
+    "url": "/static/js/2.e4c31876.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4c31876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a4a54d0d0262e6b427",
+    "url": "/static/js/main.f03a7d7f.chunk.js"
+  },
+  {
+    "revision": "46e1ce656ac57e3635d1",
+    "url": "/static/js/runtime-main.876764e1.js"
+  }
+]);
